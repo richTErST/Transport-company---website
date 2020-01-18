998347261793086b37e355a6ad78753217697c2e
richTErST <46261940+richTErST@users.noreply.github.com>
2020-01-18 13:06:06 +0100

    js added
    icon & slide added to index

---
+++ js/imgSlider.js
@@ -0,0 +1,38 @@
+			var number = Math.floor(Math.random()*5)+1;
+		
+			var timer1 = 0;
+			var timer2 = 0;
+			
+
+			function setSlide(slideNr)
+			{
+			
+				clearTimeout(timer1);
+				clearTimeout(timer2);
+				number = slideNr - 1;
+				
+				hide();
+				setTimeout("changeSlide()", 2000);
+			}
+		
+				
+			function hide()
+			{
+				$("#slider").fadeOut(2000);
+			}
+		
+		
+			function changeSlide()
+			{
+			
+				number++;
+				if (number>5) number=1;
+			
+				var imgFile = "<img src=\"img/slides/slajd"+ number + ".jpg\" />";
+				
+				document.getElementById("slider").innerHTML = imgFile;
+				$("#slider").fadeIn(2000);
+				
+				timer1 = setTimeout("changeSlide()", 8500);
+				timer2 = setTimeout("hide()", 6500);
+			}
