4753d5b15a30053a6b4bc456abe46f649d1a9ccf
richTErST <46261940+richTErST@users.noreply.github.com>
2020-01-28 20:35:11 +0100

    refactored code

--- js/imgSlider.js
+++ js/imgSlider.js
@@ -1,38 +1,36 @@
-			var number = Math.floor(Math.random()*5)+1;
+var number = Math.floor(Math.random()*5)+1;
 		
-			var timer1 = 0;
-			var timer2 = 0;
+var timer1 = 0;
+var timer2 = 0;
 			
 
-			function setSlide(slideNr)
-			{
-			
-				clearTimeout(timer1);
-				clearTimeout(timer2);
-				number = slideNr - 1;
+	function setSlide(slideNr)
+	{
+		clearTimeout(timer1);
+		clearTimeout(timer2);
+		number = slideNr - 1;
 				
-				hide();
-				setTimeout("changeSlide()", 2000);
-			}
+		hide();
+		setTimeout("changeSlide()", 2000);
+	}
 		
 				
-			function hide()
-			{
-				$("#slider").fadeOut(2000);
-			}
+	function hide()
+	{
+		$("#slider").fadeOut(2000);
+	}
 		
 		
-			function changeSlide()
-			{
+	function changeSlide()
+	{
+		number++;
+		if (number>5) number=1;
 			
-				number++;
-				if (number>5) number=1;
-			
-				var imgFile = "<img src=\"img/slides/slajd"+ number + ".jpg\" />";
+		var imgFile = "<img src=\"img/slides/slajd"+ number + ".jpg\" />";
 				
-				document.getElementById("slider").innerHTML = imgFile;
-				$("#slider").fadeIn(2000);
+		document.getElementById("slider").innerHTML = imgFile;
+		$("#slider").fadeIn(2000);
 				
-				timer1 = setTimeout("changeSlide()", 8500);
-				timer2 = setTimeout("hide()", 6500);
-			}
+		timer1 = setTimeout("changeSlide()", 8500);
+		timer2 = setTimeout("hide()", 6500);
+	}
